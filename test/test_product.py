46603a5badfe01f16830a8d7da97ca0048ecdb2d
Thijs van de Laar <thijsvdlaar@hotmail.com>
2020-09-16 16:36:07 +0200

    use pytest for testing

--- test/test_product.py
+++ test/test_product.py
@@ -1,34 +1,29 @@
-import unittest
-import product as p
+from product import *
 
 
-class ProductTest(unittest.TestCase):
+class TestProduct:
     def test_initialize_product(self):
-        product = p.Product(1, 2)
-        self.assertEqual(product.days_remaining, 1)
-        self.assertEqual(product.quality, 2)
+        product = Product(1, 2)
+        assert product.days_remaining == 1
+        assert product.quality == 2
 
     def test_tick_fresh(self):
         # Quality for a fresh product is decreased by 1
-        product = p.Product(2, 3)
+        product = Product(2, 3)
         product.tick()
-        self.assertEqual(product.days_remaining, 1)
-        self.assertEqual(product.quality, 2)
+        assert product.days_remaining == 1
+        assert product.quality == 2
 
     def test_tick_past_due(self):
         # Quality for a product past due is decreased by 2
-        product = p.Product(0, 3)
+        product = Product(0, 3)
         product.tick()
-        self.assertEqual(product.days_remaining, -1)
-        self.assertEqual(product.quality, 1)
+        assert product.days_remaining == -1
+        assert product.quality == 1
 
     def test_tick_rotten(self):
         # Quality for a rotten product remains 0
-        product = p.Product(2, 0)
+        product = Product(2, 0)
         product.tick()
-        self.assertEqual(product.days_remaining, 1)
-        self.assertEqual(product.quality, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert product.days_remaining == 1
+        assert product.quality == 0
