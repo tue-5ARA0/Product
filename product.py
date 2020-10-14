a617d03abf9f904386cfbf080748308c55122d1b
Thijs van de Laar <thijsvdlaar@hotmail.com>
2020-10-14 13:36:39 +0200

    add example notebook

--- product.py
+++ product.py
@@ -23,16 +23,16 @@ class Product:
     #     return self
 
     # REFACTOR
-    # def tick(self):
-    #     self.days_remaining -= 1
-    #
-    #     # Product is rotten
-    #     if self.quality == 0:
-    #         return self
-    #
-    #     # Decrease quality
-    #     self.quality -= 1
-    #     if self.days_remaining <= 0:  # Product is past due
-    #         self.quality -= 1
-    #
-    #     return self
+    def tick(self):
+        self.days_remaining -= 1
+
+        # Product is rotten
+        if self.quality == 0:
+            return self
+
+        # Decrease quality
+        self.quality -= 1
+        if self.days_remaining <= 0:  # Product is past due
+            self.quality -= 1
+
+        return self
